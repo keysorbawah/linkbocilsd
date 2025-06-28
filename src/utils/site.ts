4e3f67c38f4d816597d4a6a678b56c29cbfa2b83
keysorbawah <keysorbawah@gmail.com>
2025-06-29 03:02:59 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Link Bocil Sd';
-export const desk = 'Video bokep indo jepang viral ngentot colmek orgasme bocil smp jilbab gangbang smk.';
+export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
 export const url = 'https://linkbocilsd.pages.dev';
-export const terbit = '2025-06-25T00:00:00Z';
+export const terbit = '2025-06-28T00:01:03Z';
